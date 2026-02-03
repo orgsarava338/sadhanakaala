41f6b12dad343193b60af08aeca090d38d52179f
sarava338 <sarava338@gmail.com>
2026-02-03 23:05:09 +0530

    enabled cors

--- server/src/main/java/orgsarava338/sadhanakaala/infra/config/SecurityConfigDEV.java
+++ server/src/main/java/orgsarava338/sadhanakaala/infra/config/SecurityConfigDEV.java
@@ -1,10 +1,17 @@
 package orgsarava338.sadhanakaala.infra.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import orgsarava338.sadhanakaala.constants.ApiConstants;
 
@@ -13,16 +20,33 @@ import orgsarava338.sadhanakaala.constants.ApiConstants;
 public class SecurityConfigDEV {
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173" // web UI local
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults()) // enable cors
                 .csrf(csrf -> csrf.disable()) // disable CSRF for dev purposes
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // swagger endpoints
                         .requestMatchers(ApiConstants.AUTH_API).permitAll() // authentication endpoints
                         .requestMatchers(ApiConstants.HEALTH_API).permitAll() // health check endpoint
-
                         .anyRequest().authenticated()) // all other endpoints require authentication
-
                 .httpBasic(basic -> basic.disable()) // disable default basic auth popup
                 .formLogin(form -> form.disable()); // disable default form login
 
--- server/src/main/java/orgsarava338/sadhanakaala/infra/config/SecurityConfigProd.java
+++ server/src/main/java/orgsarava338/sadhanakaala/infra/config/SecurityConfigProd.java
@@ -1,23 +1,45 @@
 package orgsarava338.sadhanakaala.infra.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import orgsarava338.sadhanakaala.constants.ApiConstants;
+import orgsarava338.sadhanakaala.constants.HeaderConstants;
 
 @Configuration
 @Profile("prod")
 public class SecurityConfigProd {
 
     @Bean
+    CorsConfigurationSource configurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of());
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of(HeaderConstants.AUTHORIZATION));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults()) // enable cors
+                .csrf(csrf -> csrf.disable()) // disable csrf
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(ApiConstants.AUTH_API).permitAll() // authentication endpoints
                         .requestMatchers(ApiConstants.HEALTH_API).permitAll() // health check endpoint
